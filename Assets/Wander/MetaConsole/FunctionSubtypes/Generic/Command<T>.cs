ec8e82cce0fe8715e81c8b67c80aa7d7aa0f7648
Maxwell <maxwellhunter@protonmail.com>
2018-05-22 04:11:53 +1200

    Added generic implementation of Command

--- Assets/Wander/MetaConsole/FunctionSubtypes/Generic/Command<T>.cs
+++ Assets/Wander/MetaConsole/FunctionSubtypes/Generic/Command<T>.cs
@@ -1,5 +1,8 @@
 using System;
 using UnityEngine.Assertions;
+using System.Reflection;
+using System.Linq;
+using Wander.Parsing;
 
 namespace Wander.MetaConsole
 {
@@ -7,6 +10,7 @@ namespace Wander.MetaConsole
 	{
 		public delegate void Signature(T arg1 = default(T));
 		Signature function;
+		object[] defaultValues;
 
 		public 
 		Command(string name, string desc, Signature function)
@@ -14,11 +18,23 @@ namespace Wander.MetaConsole
 		{
 			Assert.IsNotNull(function);
 			this.function = function;
+
+			var parameters = function.Method.GetParameters();
+			defaultValues = parameters.Select(p => p.DefaultValue).ToArray();
+			concreteArgsCount = parameters.Count(p => !p.IsOptional);
+			concreteArgsCount = parameters.Count(p => p.IsOptional);
 		}
 
 		protected override void InvokeInternal(string[] args)
 		{
-			// function(args);
+			try {
+				switch(args.Length) {
+					case 0: function((T) defaultValues[0]); break;
+					case 1: function(StringParser.Parse<T>(args[0])); break;
+				}
+			} catch (Exception e){
+				Console.WriteLine("{0}, usage {1}", e.Message, Usage);
+			}
 		}
 	}
 }
