1554239dfb83468844f41f23f696fed01f084bd7
Maxwell <maxwellhunter@protonmail.com>
2018-05-28 01:13:33 +1200

    Fixed whitespace

--- Assets/Wander/MetaConsole/CommandLine.cs
+++ Assets/Wander/MetaConsole/CommandLine.cs
@@ -8,130 +8,130 @@ using UnityEngine;
 
 namespace Wander.MetaConsole
 {
-	public static class CommandLine
-	{
-		public class WriteEvent : UnityEvent<string> {}
-		
-		/// Invoked when Write or WriteLine is called with the message supplied.
-		public static readonly WriteEvent OnWrite = new WriteEvent();
-		public static ArchiveFile Archive { get; private set; }
-		public static IReadOnlyCollection<ICommand> Commands 
-			{ get { return commands.AsReadOnly(); } }
+  public static class CommandLine
+  {
+    public class WriteEvent : UnityEvent<string> {}
+    
+    /// Invoked when Write or WriteLine is called with the message supplied.
+    public static readonly WriteEvent OnWrite = new WriteEvent();
+    public static ArchiveFile Archive { get; private set; }
+    public static IReadOnlyCollection<ICommand> Commands 
+      { get { return commands.AsReadOnly(); } }
 
-		// Used by command variables so they don't write to the archive file because
-		// executing the archive file changed their values.
-		public static bool ShouldWriteArchive { get; private set; }
+    // Used by command variables so they don't write to the archive file because
+    // executing the archive file changed their values.
+    public static bool ShouldWriteArchive { get; private set; }
 
-		static List<ICommand> commands = new List<ICommand>();
+    static List<ICommand> commands = new List<ICommand>();
 
-		static CommandLine()
-		{
-			InitCommandLine();
-		}
+    static CommandLine()
+    {
+      InitCommandLine();
+    }
 
-		public static void InitCommandLine()
-		{
+    public static void InitCommandLine()
+    {
 
-			commands.Clear();
+      commands.Clear();
 
-			UnityEngine.Debug.Log("Command line init.");
-			// Add static methods tagged with the AutoCommand attribute.
-			// The AutoCommandHandler is not a fun read unless you enjoy hard core
-			// type abuse.
-			commands.AddRange(AutoCommandHandler.GenerateAutoCommandsList());
-			commands.AddRange(BuiltInVariables.GetVariableList());
+      UnityEngine.Debug.Log("Command line init.");
+      // Add static methods tagged with the AutoCommand attribute.
+      // The AutoCommandHandler is not a fun read unless you enjoy hard core
+      // type abuse.
+      commands.AddRange(AutoCommandHandler.GenerateAutoCommandsList());
+      commands.AddRange(BuiltInVariables.GetVariableList());
 
-			Archive = new ArchiveFile("archive.cfg");
-			ShouldWriteArchive = false;
-			Archive.Execute();
-			ShouldWriteArchive = true;
-		}
+      Archive = new ArchiveFile("archive.cfg");
+      ShouldWriteArchive = false;
+      Archive.Execute();
+      ShouldWriteArchive = true;
+    }
 
-		public static T AddCommand<T>(T command) where T : ICommand
-		{
-			Assert.IsFalse(HasCommand(command.Name), command.Name + " is not unique.");
-			Assert.IsFalse(command == null, "Command supplied cannot be null.");
+    public static T AddCommand<T>(T command) where T : ICommand
+    {
+      Assert.IsFalse(HasCommand(command.Name), command.Name + " is not unique.");
+      Assert.IsFalse(command == null, "Command supplied cannot be null.");
 
-			commands.Add(command);
-			return command;
-		}
+      commands.Add(command);
+      return command;
+    }
 
-		/// Get the command corresponding to it's name.
-		public static ICommand GetCommand(string commandName)
-		{
-			if (String.IsNullOrEmpty(commandName)) return null;
-			return commands.Find(c => c.Name == commandName);
-		}
+    /// Get the command corresponding to it's name.
+    public static ICommand GetCommand(string commandName)
+    {
+      if (String.IsNullOrEmpty(commandName)) return null;
+      return commands.Find(c => c.Name == commandName);
+    }
 
-		/// Check if a command by a given name already exists.
-		public static bool HasCommand(string commandName)
-		{
-			if (String.IsNullOrEmpty(commandName)) return false;
-			return GetCommand(commandName) != null;
-		}
-		
-		/// Execute a line of input, such as from a command console or config file. 
-		/// Keep in mind this should be a line of input, any linebreaks will be
-		/// passed to commands, not executed as seperate lines.
-		/// You can seperate commands by using a ;
-		public static void Execute(string input)
-		{
-			if (String.IsNullOrEmpty(input)) return;
-			if (input.StartsWith("#")) return; // Is a comment.
-			
-			var inputs = input.Split(';');
-			
-			foreach (var i in inputs) {
-				CommandLine.WriteLine("$ " + i);
-				var tokens = Tokenize(i);
-				ExecuteArgs(tokens);
-			}
-		}
+    /// Check if a command by a given name already exists.
+    public static bool HasCommand(string commandName)
+    {
+      if (String.IsNullOrEmpty(commandName)) return false;
+      return GetCommand(commandName) != null;
+    }
+    
+    /// Execute a line of input, such as from a command console or config file. 
+    /// Keep in mind this should be a line of input, any linebreaks will be
+    /// passed to commands, not executed as seperate lines.
+    /// You can seperate commands by using a ;
+    public static void Execute(string input)
+    {
+      if (String.IsNullOrEmpty(input)) return;
+      if (input.StartsWith("#")) return; // Is a comment.
+      
+      var inputs = input.Split(';');
+      
+      foreach (var i in inputs) {
+        CommandLine.WriteLine("$ " + i);
+        var tokens = Tokenize(i);
+        ExecuteArgs(tokens);
+      }
+    }
 
-		/// Execute pre-tokenized input.
-		public static void ExecuteArgs(params string[] args)
-		{
-			if (args == null || args.Length < 1) return;
+    /// Execute pre-tokenized input.
+    public static void ExecuteArgs(params string[] args)
+    {
+      if (args == null || args.Length < 1) return;
 
-			if (!HasCommand(args[0])) {
-				WriteLine("{0}: command not found.", args[0]);
-				return;
-			}
+      if (!HasCommand(args[0])) {
+        WriteLine("{0}: command not found.", args[0]);
+        return;
+      }
 
-			var command = GetCommand(args[0]);
-			var skippedArgs = args.Skip(1).ToArray();
+      var command = GetCommand(args[0]);
+      var skippedArgs = args.Skip(1).ToArray();
 
-			command.Invoke(skippedArgs);
-		}
+      command.Invoke(skippedArgs);
+    }
 
-		/// Write a message to any OnWrite listeners.
-		public static void Write(string message)
-		{
-			if (!String.IsNullOrEmpty(message))
-				OnWrite.Invoke(message);
-		}
+    /// Write a message to any OnWrite listeners.
+    public static void Write(string message)
+    {
+      if (!String.IsNullOrEmpty(message))
+        OnWrite.Invoke(message);
+    }
 
-		/// Write a message to any OnWrite listeners.
-		public static void WriteLine(string message = "")
-		{
-			Write(message + "\n");
-		}
+    /// Write a message to any OnWrite listeners.
+    public static void WriteLine(string message = "")
+    {
+      Write(message + "\n");
+    }
 
-		/// Write a message to any OnWrite listeners.
-		public static void Write(string format, params object[] args)
-		{
-			Write(String.Format(format, args));
-		}
+    /// Write a message to any OnWrite listeners.
+    public static void Write(string format, params object[] args)
+    {
+      Write(String.Format(format, args));
+    }
 
-		/// Write a message to any OnWrite listeners.
-		public static void WriteLine(string format, params object[] args)
-		{
-			WriteLine(String.Format(format, args));
-		}
-		
-		public static string[] Tokenize(string input)
-		{
-			return input.Split(' ');
-		}
-	}
+    /// Write a message to any OnWrite listeners.
+    public static void WriteLine(string format, params object[] args)
+    {
+      WriteLine(String.Format(format, args));
+    }
+    
+    public static string[] Tokenize(string input)
+    {
+      return input.Split(' ');
+    }
+  }
 }
